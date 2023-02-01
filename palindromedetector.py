9529d4aab894d13044020be130d3c7ed4453c576
BrianENason <94876170+BrianENason@users.noreply.github.com>
2023-02-01 09:05:55 -0800

    Update palindromedetector.py
    Changed the test input type to a dictionary with the word/phrase as the key and the T/F of if it is actually a palindrome as the value for ease of testing. Also updated the output string formatting, added a standard "divider", and added a colon and a space to the end of the user input prompt.

--- palindromedetector.py
+++ palindromedetector.py
@@ -1,38 +1,49 @@
 ################################################################################
 # Initialization(s)
-#  1) testRun = 
+#  1) runTests = 
 #    When set to True, it will iterate through the testList below
 #    When set to False, it will call for the user to enter a string
 ################################################################################
 
-testRun = True
+runTests = False
 
 ################################################################################
 # Special List(s)
 #  1) specialCharacter - This list is populated with common punctuation for the program to ignore
 #  2) testList - A list of special words/sentences as test cases
+#  3) testDict - A Dictionary of words/phrases to test with their expected outcome as the value
 ################################################################################
 
 specialCharacter = [' ', ',', '.', '-', '!', '\'', '\"', '@', '#', '$', '%', ';', ':']
+
 testList = ["what!", "Racecar!", "raceCar", " rise to vote, sir", "goog", "", 
             "         ", "tattarrattat", "Madam, in Eden, I'm Adam", "12321", 
             "1A2b3C3B2a1", "too hot to hoot!", "tO oHoTt o,HoOt!", " 1 2,3.4-5!6@5#4$3%2;1:"]
 
+testDict = {"what!":False, "Racecar!":True, "raceCar":True, " rise to vote, sir":True, 
+            "goog":True, "":False, "         ":False, "tattarrattat":True, 
+            "Madam, in Eden, I'm Adam":True, "12321":True, "1A2b3C3B2a1":True, 
+            "too hot to hoot!":True, "tO oHoTt o,HoOt!":True, " 1 2,3.4-5!6@5#4$3%2;1:":True}
+
 ################################################################################
 # Output Format Function(s)
 #  1) printDivider - When called, it prints a standard divider between entries
-#  2) outputToUser - provides a standard way to output each of the 3 possible cases:
+#  2) printSeparator - When called, it prints a standard separator between entries
+#  3) outputToUser - provides a standard way to output each of the 3 possible cases:
 #    a. Word IS a palindrome
 #    b. Word is NOT a palindrome
 #    c. Word is not valid for analysis
 ################################################################################
 
 def printDivider():  
-  print('/', ('-' * 100), '/')
+  print('\n/', ('-' * 80), '/')
 
-def outputToUser(word, isAPal):
-  printDivider()
-  print("\nYour word \n\t\"{}\"\n{} a palindrome\n".format(word, isAPal))
+def printSeparator():  
+  print('\n', ('~' * 40))
+
+def outputToUser(word, isAPal):  
+  printDivider()  
+  print("\nYour word: \"{}\" {} a palindrome".format(word, isAPal))  
 
 ################################################################################
 # Main Logic Function(s)
@@ -51,7 +62,7 @@ def clearSpecialChar(word):
 def checkForPal (cleanString):
   i = 0  
   k = (len(cleanString) - 1)
-  while (k >= i):
+  while (k > i):
     if(cleanString[k] == cleanString[i]):
       k -= 1
       i += 1
@@ -71,7 +82,7 @@ def fullPalindromeCheck(word):
   blankWord = firstPalindromeCheck(word)
   if not blankWord:
     outputToUser(word, "is not a valid word, and therefore is NOT")
-  else:
+  else:    
     palindromeWord = nextPalindromeCheck(blankWord)
     if not palindromeWord:
       outputToUser(word, "is NOT")
@@ -89,22 +100,29 @@ def nextPalindromeCheck(word):
 ################################################################################
 # Test Function(s)
 #  1) runTest() - This will iterate through the test list of words and pass them through the full palindrome check
+#  2) runDictTest() - This will use the testDict to pass the keys (word/phrase to test) and make sure the expected answer (value) is returned
 ################################################################################
 
-def runTest():
-  for word in testList:
-    # palindromeCheck(word)
+def runListTest():
+  for word in testList:    
     fullPalindromeCheck(word)
+
+def runDictTest():
+  for word in testDict:
+    fullPalindromeCheck(word)
+    printSeparator()
+    print("\nThe correct answer is {}".format(testDict[word]))
 
 ################################################################################
 # Program Main
 ################################################################################
 
 def main():
-  if (testRun == True):
-    runTest()
+  if (runTests == True):
+    # runListTest()  # Old form of test using a list of values
+    runDictTest()
   else:
-    userWord = input("Please enter a word to check")
+    userWord = input("Please enter a word to check: ")
     # palindromeCheck(userWord)
     fullPalindromeCheck(userWord)
 
